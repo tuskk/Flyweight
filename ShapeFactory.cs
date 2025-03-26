ebab05f2f1799f87b49e1be818b3f51835ea19f6
Mega <v.v.humeniuk@student.khai.edu>
2025-03-26 19:58:35 +0200

    fix1

--- ShapeFactory.cs
+++ ShapeFactory.cs
@@ -8,12 +8,10 @@ namespace Flyweight
             {
                 case ShapeType.LINE:
                     return new Line(x, y, x + width, y + height, color);
-                case ShapeType.OVAL_FILL:
-                    return new Oval(x, y, width, height, color, true);
-                  case ShapeType.OVAL_NOFILL:
-                    return new Oval(x, y, width, height, color, false);
-                case ShapeType.CRESCENT:
-                    return new Crescent(x, y, width, height, color);
+                case ShapeType.Crescent_FILL:
+                    return new Crescent(x, y, width, height, color, true);
+                  case ShapeType.Crescent_NOFILL:
+                    return new Crescent(x, y, width, height, color, false);
                 default:
                     throw new ArgumentException("Invalid shape type");
             }
